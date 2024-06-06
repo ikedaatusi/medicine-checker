a6ef7e421f2eb2b1dcb05ff5f06d70b77c872393
ikedaatusi <146809265+ikedaatusi@users.noreply.github.com>
2024-06-06 23:52:52 +0900

    ラインbot追加

---
+++ app/controllers/webhook_controller.rb
@@ -0,0 +1,13 @@
+class WebhookController < ApplicationController
+  def kokodayo
+    message = {
+            "type": "text",
+            "text": "メッセージ"
+           }
+            client = Line::Bot::Client.new { |config|
+              config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+              config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+           }
+  response = client.push_message(ENV["LINE_USER_ID"], message)
+  end
+end
