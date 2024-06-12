db7667fdb2b712dac07dd69b10a9d4c390fe87df
ikedaatusi <146809265+ikedaatusi@users.noreply.github.com>
2024-06-12 19:17:39 +0900

    薬一覧のcss調整

--- app/controllers/already_taken_controller.rb
+++ app/controllers/already_taken_controller.rb
@@ -3,7 +3,10 @@ class AlreadyTakenController < ApplicationController
     #服用期間が終了した薬を表示させる
     @day = Date.today
     special_drug_ids = current_user.drugs.where.not(start_time: nil).pluck(:id)
-    @drugs = current_user.drugs.includes(:take_times).where(id: special_drug_ids).where('end_time < ?', Date.today)
+    @search_results = current_user.drugs.includes(:take_times).where(id: special_drug_ids).where('end_time < ?', Date.today)
+    @q = @search_results.ransack(params[:q])
+    @drugs =  @q.result(distinct: true).includes(:take_times).order(created_at: :desc).page(params[:page]).per(12)
+    
   end
 
   def show
