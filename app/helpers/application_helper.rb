d74053cb49c509daa81af38755694d43ae21f432
ikedaatusi <146809265+ikedaatusi@users.noreply.github.com>
2024-06-21 18:00:10 +0900

    Xシェア機能

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,5 +21,35 @@ module ApplicationHelper
         end
       end
 
-      
+      def default_meta_tags
+        {
+          site: 'Pluspo',
+          title: '都内スポーツ施設の横断検索サービス',
+          reverse: true,
+          charset: 'utf-8',
+          description: 'Pluspoを使えば「スポーツ・日時・場所」の好きな組み合わせで自由にスポーツ施設を検索できます。',
+          keywords: 'スポーツ,スポーツ施設,東京',
+          canonical: request.original_url,
+          separator: '|',
+          og: {
+            site_name: :site,
+            title: :title,
+            description: :description,
+            type: 'website',
+            url: request.original_url,
+            image: image_url('mdc.png'), # 配置するパスやファイル名によって変更すること
+            local: 'ja-JP'
+          },
+          # Twitter用の設定を個別で設定する
+          twitter: {
+            card: 'summary_large_image', # Twitterで表示する場合は大きいカードにする
+            site: '@', # アプリの公式Twitterアカウントがあれば、アカウント名を書く
+            image: image_url('mdc.png'), # 配置するパスやファイル名によって変更すること
+            url: request.original_url
+          }
+        }
+      end
+      def twitter_share_link
+        link_to 'Twitter', "https://twitter.com/share?url=#{CGI.escape(request.original_url)}&text=#{CGI.escape('都内スポーツ施設の横断検索サービス')}", title: 'Twitter', target: '_blank', class: 'twitter-share-button'
+      end
 end
