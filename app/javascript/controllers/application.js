db956c8dbbf5df31eb7b6d59396c0bdd436948a1
ikedaatusi <146809265+ikedaatusi@users.noreply.github.com>
2024-07-06 20:48:43 +0900

    リファクタリング

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -3,9 +3,6 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
-import { Tabs } from "tailwindcss-stimulus-components"
-application.register('tabs', Tabs)
-
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
