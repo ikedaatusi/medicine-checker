182ffa8f98af67cdb6846b899172473c6a5dc7e5
ikedaatusi <146809265+ikedaatusi@users.noreply.github.com>
2024-08-21 22:43:18 +0900

    リファクタリング

--- app/controllers/currently_taking_controller.rb
+++ app/controllers/currently_taking_controller.rb
@@ -7,6 +7,5 @@ class CurrentlyTakingController < ApplicationController
     @search_results = current_user.drugs.includes(:take_times).where(id: special_drug_ids).where('start_time <= ?', Date.today).where('end_time >= ?', Date.today)
     @q = @search_results.ransack(params[:q])
     @drugs =  @q.result(distinct: true).includes(:take_times).order(created_at: :desc).page(params[:page]).per(12)
-    
   end
 end
